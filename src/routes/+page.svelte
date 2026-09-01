c098fb60ce94a395a6c8ada3a6f6a64f28995a89
Kevin Ruff <21102717+kevroadrunner@users.noreply.github.com>
2026-09-01 13:55:40 +0200

    Update root nav.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,41 +3,38 @@
   import type { RouteId } from '$app/types';
   import title from '$lib/title';
   import meta from '$lib/data/meta';
-  import User from '@iconify-svelte/mdi/smiley-kiss-outline';
-  import Code from '@iconify-svelte/mdi/code';
-  import Console from '@iconify-svelte/mdi/console';
-  import { cn } from '$lib/utils';
+  import AboutIcon from '@iconify-svelte/mdi/smiley-kiss-outline';
+  import ExperienceIcon from '@iconify-svelte/mdi/briefcase-outline';
+  import ProjectsIcon from '@iconify-svelte/mdi/file-document-multiple-outline';
+  import SkillsIcon from '@iconify-svelte/mdi/code';
+  import ToolsIcon from '@iconify-svelte/mdi/console';
 
   type NavItem = {
     label: string;
-    href: RouteId;
-    icon: typeof User;
-    disabled?: true;
+    href: RouteId | string;
+    icon: typeof AboutIcon;
   };
 
   const navItems: NavItem[] = [
-    { label: 'About', href: '/about', icon: User },
-    { label: 'Code', href: '/code', icon: Code, disabled: true },
-    { label: 'Tools', href: '/tools', icon: Console },
+    { label: 'About', href: '/about', icon: AboutIcon },
+    { label: 'Experience', href: `${meta.social.cv}experience/`, icon: ExperienceIcon },
+    { label: 'Projects', href: `${meta.social.cv}projects/`, icon: ProjectsIcon },
+    { label: 'Skills', href: `${meta.social.cv}skills/`, icon: SkillsIcon },
+    { label: 'Tools', href: '/tools', icon: ToolsIcon },
   ];
 </script>
 
 <svelte:head>
   <title>{title()}</title>
-  <meta name="description" content={`Personal website of ${meta.name}`} />
+  <meta name="description" content={`This is me, ${meta.name}!`} />
 </svelte:head>
 
 <section class="flex flex-1 items-center justify-center px-2 py-4 sm:px-8 sm:py-6">
   <nav class="flex min-w-[67vw] flex-col">
     {#each navItems as Link (Link.href)}
-      {@const component = !Link.disabled ? 'a' : 'span'}
-      <svelte:element
-        this={component}
-        href={!Link.disabled ? resolve(Link.href) : undefined}
-        class={cn('flex border-b py-6 last:border-0 sm:py-8', {
-          'transition-colors hover:text-primary': !Link.disabled,
-          'text-muted': Link.disabled,
-        })}
+      <a
+        href={Link.href.startsWith('/') ? resolve(Link.href as RouteId) : Link.href}
+        class="flex border-b py-6 transition-colors last:border-0 hover:text-primary sm:py-8"
       >
         <div
           class="flex items-center gap-4 text-4xl font-medium tracking-tight sm:gap-6 sm:text-6xl lg:gap-8 lg:text-7xl"
@@ -45,7 +42,7 @@
           <Link.icon class="size-10 sm:size-15 lg:size-18" />
           {Link.label}
         </div>
-      </svelte:element>
+      </a>
     {/each}
   </nav>
 </section>
